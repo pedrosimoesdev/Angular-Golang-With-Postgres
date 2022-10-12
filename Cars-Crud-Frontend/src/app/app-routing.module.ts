06e73fda89d3007f6c332686d4d7c09156918615
Pedro Developer <pedrosimoesdeveloper@gmail.com>
2022-10-12 21:47:38 +0100

    fixed issue with year

--- Cars-Crud-Frontend/src/app/app-routing.module.ts
+++ Cars-Crud-Frontend/src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { CarsComponent } from './Cars/cars.component';
 import {CarsCreateComponent} from './Cars/cars-create/cars-create.component'
 import {CarsEditComponent} from './Cars/cars-edit/cars-edit.component'
 
-const routes: Routes = [ 
+const routes: Routes = [
   { path: "", redirectTo: "/cars", pathMatch: "full" },
   { path: "cars", component: CarsComponent },
   { path: "cars/create", component: CarsCreateComponent },
-  { path: "cars/edit/:id/:name/:model/:year'", component: CarsEditComponent }
+  { path: "cars/edit/:id/:name/:model/:year", component: CarsEditComponent }
 ];
 
 @NgModule({
